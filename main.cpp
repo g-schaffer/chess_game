031c884634153f27db73de7b20ecb653397f5c61
g-schaffer <guillaume.schaffer@protonmail.com>
2021-07-14 21:42:44 +0200

    we can now move a pawn !

--- main.cpp
+++ main.cpp
@@ -1,33 +1,89 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics.hpp>
+#include <string>
 
+using namespace sf;
 
 int main() {
-	/*std::cout << "Hello World!";*/
 
-    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML window");
+    RenderWindow window(VideoMode(600, 600), "SFML window");
 
 
-    sf::Texture texture;
-    if(!texture.loadFromFile("images/board.png"))
+    Texture board, pawns;
+    if(!board.loadFromFile("images/board.png"))
     	return EXIT_FAILURE;
 
-    sf::Sprite sprite(texture);
+    IntRect size(0,0,50,55);
 
 
+    if(!pawns.loadFromFile("images/pawns.jpg", size))
+    	return EXIT_FAILURE;
+
+
+    board.setSmooth(true);
+    pawns.setSmooth(true);
+
+    Sprite sprite(board);
+
+    Sprite rook(pawns);
+
+    rook.setOrigin(-10,-10);
+
+    bool mouse_pressed_on_pawn = false;
+    Vector2i decalage_mouse_pawn;
+
+	
     while(window.isOpen()) {
 
 
-    	sf::Event event;
+    	Event event;
     	while(window.pollEvent(event)) {
-    		if(event.type == sf::Event::Closed)
-    			window.close();
+
+    		switch(event.type) {
+
+
+    			case Event::Closed:
+    				
+	    			window.close();
+    
+
+	    		case Event::MouseButtonPressed:
+
+	    			std::cout << "button mouse pressed\n";
+	    			std::cout << "x pos " + std::to_string(Mouse::getPosition(window).x) + "\n";
+
+	    			// if mouse is on a pawn
+	    			if(rook.getGlobalBounds().contains(Mouse::getPosition(window).x, Mouse::getPosition(window).y)){
+	    				std::cout << "pawn selected \n";
+	    				mouse_pressed_on_pawn = true;
+	    				decalage_mouse_pawn = Vector2i(Mouse::getPosition(window).x - rook.getPosition().x,Mouse::getPosition(window).y - rook.getPosition().y);
+	    			}
+	    			break;
+
+
+	    		case Event::MouseButtonReleased:
+
+	    			// if mouse is in sprite
+    				std::cout << "button mouse released \n";
+    				mouse_pressed_on_pawn = false;
+
+	    			
+
+
+	    		case Event::MouseMoved:
+	    			if(mouse_pressed_on_pawn) {
+	    				std::cout << std::to_string(Mouse::getPosition(window).x)+"\n";
+	    				rook.setPosition(Mouse::getPosition(window).x - decalage_mouse_pawn.x, Mouse::getPosition(window).y - decalage_mouse_pawn.y);
+	    			}
+    		}
+
     	}
 
 
     	window.clear();
     	window.draw(sprite);
+    	window.draw(rook);
     	window.display();
     }
 
