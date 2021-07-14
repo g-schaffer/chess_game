369cbc2b00c35fc6cae50d602a790dda14ce957a
g-schaffer <guillaume.schaffer@protonmail.com>
2021-07-14 15:15:01 +0200

    rm hello world display board

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+
 
 int main() {
-	std::cout << "Hello World!";
-	return 0;
+	/*std::cout << "Hello World!";*/
+
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML window");
+
+
+    sf::Texture texture;
+    if(!texture.loadFromFile("images/board.png"))
+    	return EXIT_FAILURE;
+
+    sf::Sprite sprite(texture);
+
+
+    while(window.isOpen()) {
+
+
+    	sf::Event event;
+    	while(window.pollEvent(event)) {
+    		if(event.type == sf::Event::Closed)
+    			window.close();
+    	}
+
+
+    	window.clear();
+    	window.draw(sprite);
+    	window.display();
+    }
+
+	return EXIT_SUCCESS;
 }
 
 
